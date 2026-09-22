081740347b9fc8f98ccf55602e687ded36e911fe
Nishad112-cyber <nishadguptad@gmail.com>
2026-09-22 23:17:23 +0530

    add kiya new line

--- app/page.js
+++ app/page.js
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div>
     <h1>Hellow Next.js</h1>
+    <h1>Start Learning </h1>
+    <h1>New Concept </h1>
+    </div>
   );
 }
