00cc487159e3322e52e20943bb5da9faa8aee5f8
Nishad112-cyber <nishadguptad@gmail.com>
2026-09-25 23:43:11 +0530

    styling in next.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,5 @@
 export default function About() {
+  console.log("About page running");
+  
   return <h1>About Page</h1>;
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
-
+import clsx from "clsx";
+import styles from "./Home.module.css";
 export default function Home() {
+
+  const isActive= true;
   return (
     <div>
-    <h1>Hellow Next.js</h1>
+    <h1
+      className={clsx(
+        "text-4xl",
+        isActive && "font-bold text-blue-500"
+      )}
+    >
+      Hellow Next.js</h1>
     <h1>Start Learning </h1>
     <h1>New Concept </h1>
     </div>
